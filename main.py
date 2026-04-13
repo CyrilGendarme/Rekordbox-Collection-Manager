b98cf10e487d056687720793f5ec63aa888a4777
CyrilGendarme <cyrilgendarme@gmail.com>
2026-04-13 23:23:07 +0200

    feat: add main entry point with hotkey listener and Rekordbox startup

---
+++ main.py
@@ -0,0 +1,36 @@
+"""
+Entry point for Rekordbox Phrase Cue Setter app.
+"""
+
+from gui.app import main
+from os_utils import rekordbox_process
+from tkinter import messagebox
+import threading
+import keyboard
+import os
+
+
+def close_app():
+    os._exit(0)
+
+
+def listen_for_ctrl_z():
+    keyboard.add_hotkey("ctrl+c", close_app)
+    keyboard.wait()  # Keep the thread alive to listen for hotkeys
+
+
+if __name__ == "__main__":
+
+    # Start global hotkey listener in a background thread
+    threading.Thread(target=listen_for_ctrl_z, daemon=True).start()
+
+    if not rekordbox_process.is_rekordbox_running():
+        rekordbox_process.launch_rekordbox()
+        if not rekordbox_process.wait_for_rekordbox():
+            messagebox.showerror("Error", "Failed to launch Rekordbox.")
+            exit(1)
+
+    rekordbox_process.make_rekordbox_fullscreen_on_main_screen()
+    rekordbox_process.focus_rekordbox_window()
+
+    main()
